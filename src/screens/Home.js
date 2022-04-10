f335426d90d657ebe6888bbbde3ab2b75f174bbe
7shiroi <lucky7kelvin@gmail.com>
2022-04-10 15:03:15 +0800

    feat: conditional rendering for admin

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,13 +4,15 @@ import Layout from '../components/Layout';
 import HomeHeader from '../components/HomeHeader';
 import HomeSearch from '../components/HomeSearch';
 import RecommendList from '../components/RecommendList';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {TOGGLE_LOADING} from '../helpers/utils';
 import {getCategoriesAction} from '../redux/actions/categories';
 import {getPopularVehiclesAction} from '../redux/actions/vehicles';
+import {Box, Button, ScrollView} from 'native-base';
 
 const Home = () => {
   const dispatch = useDispatch();
+  const auth = useSelector(state => state.auth);
   const fetchData = async () => {
     dispatch({type: TOGGLE_LOADING});
     await dispatch(getCategoriesAction);
@@ -23,11 +25,16 @@ const Home = () => {
   }, []);
 
   return (
-    <>
+    <ScrollView>
       <HomeHeader />
       <HomeSearch />
       <RecommendList />
-    </>
+      {auth.userData.id_role < 3 && (
+        <Box mx={5} my={5}>
+          <Button>Add Item</Button>
+        </Box>
+      )}
+    </ScrollView>
   );
 };
 
